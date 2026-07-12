9bf846081c3f9dff1c8016fb71e5b6123099ea96
john <johnandpaul42@gmail.com>
2026-07-11 23:46:34 -0500

    Temporary: raise toast z-index above lightbox for swipe diagnosis

--- src/pages/JobDetail.jsx
+++ src/pages/JobDetail.jsx
@@ -542,7 +542,7 @@ export default function JobDetail() {
       </header>
 
       {toast && (
-        <div className="fixed top-16 left-1/2 -translate-x-1/2 z-50 bg-green-600 text-white text-sm px-4 py-2 rounded-lg shadow-lg">
+        <div className="fixed top-16 left-1/2 -translate-x-1/2 z-[60] bg-green-600 text-white text-sm px-4 py-2 rounded-lg shadow-lg">
           {toast}
         </div>
       )}
